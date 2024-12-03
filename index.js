65829d866f541f59d5943cd626ae03a99d9abb56
mrhl082024 <mrhl082024@gmail.com>
2024-12-03 13:08:49 +0100

    first

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const cors = require("cors");
+const { log } = require("console");
+const app = express();
+
+app.use(cors());
+
+app.use(express.static(path.join(__dirname, "public")));
+
+const startingClasses = JSON.parse(
+  fs.readFileSync(path.join(__dirname, "json/starting_classes.json"), "utf-8")
+);
+
+app.get("/", (req, res) => {
+  res.send("");
+});
+
+//GET Json files
+app.get("/startingClasses", (req, res) => {
+  res.json(startingClasses);
+});
+
+app.listen(8000, () => {
+  console.log("Running on port: 8000");
+});
